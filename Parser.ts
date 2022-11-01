e9ac0611dd0c70cd29b4ee2741a3629e3b6d8412
Azuraerae <nougasmbakam@gmail.com>
2022-11-01 19:01:37 +0100

    More Parser Progress & start command

--- Parser.ts
+++ Parser.ts
@@ -1,40 +1,67 @@
 import axios, { Axios } from 'axios';
 
-interface filters{
+interface filters {
     minUsers: number;
     maxUsers: number;
 }
 
-type Description = {
-
-};
-
-type Response = {
-    data: Description;
+export interface Description {
+    word:       string;
+    phonetics:  Phonetic[];
+    meanings:   Meaning[];
+    license:    License;
+    sourceUrls: string[];
 }
+
+export interface License {
+    name: string;
+    url:  string;
+}
+
+export interface Meaning {
+    partOfSpeech: string;
+    definitions:  Definition[];
+    synonyms:     string[];
+    antonyms:     string[];
+}
+
+export interface Definition {
+    definition: string;
+    synonyms:   any[];
+    antonyms:   any[];
+    example?:   string;
+}
+
+export interface Phonetic {
+    audio:      string;
+    sourceUrl?: string;
+    license?:   License;
+    text?:      string;
+}
+
 
 class data {
     link: String;
     onlineMembers: number;
     totalMembers: number;
 
-    constructor(link: string, onlineMembers: number, totalMembers: number){
+    constructor(link: string, onlineMembers: number, totalMembers: number) {
         this.link = link;
         this.onlineMembers = onlineMembers;
         this.totalMembers = totalMembers;
     }
 }
 
-export class Parser{
+export class Parser {
     static synonymsLink: string = "https://api.dictionaryapi.dev/api/v2/entries/en/";
     static apiLink: string = "https://discord.com/api/v9/invites/";
     keyword: string;
     filters: filters;
 
-    private static async getDescription(word: string){
+    private static async getDescription(word: string) {
         let request: string = this.synonymsLink.concat(word.toString());
         try {
-            const { data, status } = await axios.get<Response>(
+            const { data, status } = await axios.get<Description>(
                 request,
                 {
                     headers: {
@@ -44,29 +71,33 @@ export class Parser{
             );
             return data;
         } catch (error) {
-            if (axios.isAxiosError(error)){
+            if (axios.isAxiosError(error)) {
                 console.log("Error Message: ", error.message);
                 return -1;
             } else {
-                console.log("Unhandled error: ", error.message);
+                console.log("Unhandled error");
                 return -2;
             }
         }
     }
 
-    private static getAllSynonyms(word: string): string[]{
+    private static async getAllSynonyms(word: string): Promise<string[]> {
         let synonyms: string[] = [];
-        let data: Description = this.getDescription(word)
-        
+        let data: Description | number = await this.getDescription(word)
+        if (typeof data === "number") {
+            console.log(data.toString())
+        } else {
+            
+        }
         return synonyms;
     }
 
     private static inviteProcess(invite: string): data {
         let result: data = new data("", 0, 0)
         result.link = "";
-        
+
         return result;
-    } 
+    }
 
     static fetch(): data[] {
         let results: data[] = [];
@@ -74,8 +105,8 @@ export class Parser{
         return results;
     }
 
-    constructor(keyword, filters){
-        this,keyword = keyword;
+    constructor(keyword: string, filters: filters) {
+        this.keyword = keyword;
         this.filters = filters;
     }
 }
