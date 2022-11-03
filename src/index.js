4342b1274a6f7c02415d38be3cab03e7056e419f
RedGaming98 <51047087+viciscat@users.noreply.github.com>
2022-11-03 15:42:10 +0100

    actual contribution from vic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import { Parser } from './Parser.js';
-let keyword = "Community";
-let currentParser = new Parser();
-currentParser.fetch(keyword);
+console.log("Hoi");
+import { Parser } from "./Parser";
+console.log("Parser got loaded YEEAYAYYYYA");
+const server_list = document.querySelector("#server-list");
+const form = document.querySelector("#url-finder");
+const input = document.querySelector("#keyword-in");
+form?.addEventListener("submit", e => {
+    e.preventDefault();
+    // Prevent idiots from putting nothing in the box
+    if (input?.value == "" || input?.value == null)
+        return;
+    let keyword = input.value;
+    let parser = new Parser();
+    let data = parser.fetch(keyword);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
-import { Parser } from './Parser.js'
+console.log("Hoi")
+import {Parser} from "./Parser"
+console.log("Parser got loaded YEEAYAYYYYA")
 
-let keyword: string = "Community";
+const server_list = document.querySelector<HTMLUListElement>("#server-list")
+const form = document.querySelector<HTMLFormElement>("#url-finder")
+const input = document.querySelector<HTMLInputElement>("#keyword-in")
 
-let currentParser = new Parser();
-currentParser.fetch(keyword)
+form?.addEventListener("submit", e => {
+    e.preventDefault()
+
+    // Prevent idiots from putting nothing in the box
+    if (input?.value == "" || input?.value == null) return
+
+    let keyword:string = input.value
+    let parser = new Parser()
+    let data = parser.fetch(keyword)
+})
